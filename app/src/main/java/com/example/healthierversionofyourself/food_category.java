c7aaa15a2d35974b81149872121244024c69831f
s3915185 <106006094+s3915185@users.noreply.github.com>
2022-11-17 23:33:20 +0700

    calculate points for Food

--- app/src/main/java/com/example/healthierversionofyourself/food_category.java
+++ app/src/main/java/com/example/healthierversionofyourself/food_category.java
@@ -1,17 +1,28 @@
 package com.example.healthierversionofyourself;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.ListView;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 public class food_category extends AppCompatActivity {
-
-    @Override
+    private int points = 0;
+    private TextView pointsCalculated;
+        @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_category);
+
+
+
+
         findViewById(R.id.back_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -20,5 +31,15 @@ public class food_category extends AppCompatActivity {
                 finish();
             }
         });
+    }
+    public void onCheckboxClicked(View view) {
+            CheckBox checkBox = (CheckBox)view;
+            if (checkBox.isChecked()) {
+                points = 20 + points;
+            }
+            else{
+                points = points - 20;
+            }
+            pointsCalculated.setText("Points: " + points);
     }
 }
